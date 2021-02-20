9268ebb9e8a99bb3a01fc721578b2fafac76d243
Adrian Covaci <adrian.covaci@amdaris.com>
2021-02-20 12:58:14 +0200

    Broadcasting tweetmessage to actors

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use std::time::Duration;
+
 use actor_framework::Message;
 use actor_framework::*;
 use bytes::Bytes;
-use messages::ResponseMessage;
-use reqwest;
+use messages::TweetMessage;
 
 struct ActorSpawner {
     childs: Vec<Addr<LeActeur>>,
@@ -10,10 +11,10 @@ struct ActorSpawner {
 }
 
 impl ActorSpawner {
-    fn new() -> Self {
+    async fn new() -> Self {
         Self {
             childs: Vec::new(),
-            msg_producer: MessageProducer.new().start().await.unwrap(),
+            msg_producer: MessageProducer::new().start().await.unwrap(),
         }
     }
 }
@@ -21,14 +22,14 @@ impl ActorSpawner {
 #[async_trait::async_trait]
 impl Actor for ActorSpawner {
     async fn started(&mut self, ctx: &mut Context<Self>) -> Result<()> {
-        let addr = ctx.address().send(InitializeWorkers);
+        let _ = ctx.address().send(InitializeWorkers);
         Ok(())
     }
 }
 
 #[async_trait::async_trait]
 impl Handler<InitializeWorkers> for ActorSpawner {
-    async fn handle(&mut self, ctx: &mut Context<Self>, msg: InitializeWorkers) {
+    async fn handle(&mut self, _ctx: &mut Context<Self>, _msg: InitializeWorkers) {
         let msg_producer = self.msg_producer.clone();
         let actor_ids = vec![1, 2, 3, 4, 5];
         let child_actors_futures = actor_ids.into_iter().map(move |id| LeActeur {
@@ -36,9 +37,11 @@ impl Handler<InitializeWorkers> for ActorSpawner {
             msg_producer: msg_producer.clone(),
         });
 
-        let child_actos = child_actors_futures
+        let child_actors = child_actors_futures
             .into_iter()
             .map(|actor| async { actor.start().await.unwrap() });
+        let child_actors = futures::future::join_all(child_actors).await;
+        self.childs = child_actors;
     }
 }
 
@@ -53,37 +56,105 @@ struct LeActeur {
 
 #[async_trait::async_trait]
 impl Actor for LeActeur {
-    async fn started(&mut self, ctx: &mut Context<Self>) -> Result<()> {}
+    async fn started(&mut self, ctx: &mut Context<Self>) -> Result<()> {
+        println!("started leacteur ~ {}", self.id);
+        let self_sender = ctx.address().sender();
+        let _ = self.msg_producer.send(SubscribeToProducer {
+            sender: self_sender,
+        });
+        Ok(())
+    }
 }
 
-struct MessageProducer;
+#[async_trait::async_trait]
+impl Handler<TweetMessage> for LeActeur {
+    async fn handle(&mut self, ctx: &mut Context<Self>, msg: TweetMessage) {
+        println!("leacteur with id {} got {:?}", self.id, msg);
+    }
+}
 
-fn get_message_from_chunk(bytes: Bytes) -> ResponseMessage {
+#[async_trait::async_trait]
+impl Handler<SubscribeToProducer> for MessageProducer {
+    async fn handle(&mut self, ctx: &mut Context<Self>, msg: SubscribeToProducer) {
+        self.subscribers.push(msg.sender);
+    }
+}
+
+struct SubscribeToProducer {
+    sender: Sender<TweetMessage>,
+}
+
+#[async_trait::async_trait]
+impl Message for SubscribeToProducer {
+    type Result = ();
+}
+
+struct MessageProducer {
+    subscribers: Vec<Sender<TweetMessage>>,
+}
+impl MessageProducer {
+    fn new() -> Self {
+        Self {
+            subscribers: Vec::new(),
+        }
+    }
+}
+#[async_trait::async_trait]
+impl Actor for MessageProducer {
+    async fn started(&mut self, ctx: &mut Context<Self>) -> Result<()> {
+        //Need to figure out how to effectively send messages.
+        ctx.send_interval(HandleMessages, Duration::from_millis(50));
+        Ok(())
+    }
+}
+
+#[derive(Clone)]
+struct HandleMessages;
+
+#[async_trait::async_trait]
+impl Message for HandleMessages {
+    type Result = ();
+}
+#[async_trait::async_trait]
+impl Handler<HandleMessages> for MessageProducer {
+    async fn handle(&mut self, ctx: &mut Context<Self>, msg: HandleMessages) {
+        let msg = TweetMessage::TweetText(String::from("derp"));
+        let _: Vec<_> = self
+            .subscribers
+            .iter()
+            .map(|sub| sub.send(msg.clone()))
+            .collect();
+    }
+}
+
+fn get_message_from_chunk(bytes: Bytes) -> TweetMessage {
     let mut data = String::from_utf8(bytes.to_vec()).unwrap();
     if let Some(mut index) = data.find("\"text\"") {
         index += 8;
         data.replace_range(..index, "");
         data.replace_range(data.find("\"").unwrap()..data.len(), "");
-        return ResponseMessage::TweetText(data);
+        return TweetMessage::TweetText(data);
     }
-    ResponseMessage::Halt
+    TweetMessage::Halt
 }
 
 #[tokio::main]
 async fn main() {
-    let mut res = reqwest::get("http://localhost:4000/tweets/1")
-        .await
-        .unwrap();
+    // let mut res = reqwest::get("http://localhost:4000/tweets/1")
+    //     .await
+    //     .unwrap();
 
-    let mut i = 0;
-    'looper: loop {
-        while let Some(item) = res.chunk().await.unwrap() {
-            let response = get_message_from_chunk(item);
-            println!("{:?}", response);
-            i += 1;
-            if i >= 10 {
-                break 'looper;
-            }
-        }
-    }
+    // let mut i = 0;
+    // 'looper: loop {
+    //     while let Some(item) = res.chunk().await.unwrap() {
+    //         let response = get_message_from_chunk(item);
+    //         println!("{:?}", response);
+    //         i += 1;
+    //         if i >= 10 {
+    //             break 'looper;
+    //         }
+    //     }
+    // }
+    let parent = ActorSpawner::new().await.start().await.unwrap();
+    parent.wait_for_stop().await;
 }
--- src/messages.rs
+++ src/messages.rs
@@ -1,5 +1,12 @@
+use crate::Message;
+
 #[derive(Debug, Clone)]
-pub enum ResponseMessage {
+pub enum TweetMessage {
     TweetText(String),
     Halt,
+}
+
+#[async_trait::async_trait]
+impl Message for TweetMessage {
+    type Result = ();
 }
